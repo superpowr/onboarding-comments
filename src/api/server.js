ad61ba2f78fafa61327fe5c2b24d665af0afcc99
Will Dembinski <willdembinski@gmail.com>
2017-01-16 02:29:22 -0800

    Some ascii updates

--- src/api/server.js
+++ src/api/server.js
@@ -1,3 +1,6 @@
+const Promise            = require('bluebird');
+const colors             = require('colors');
+const figlet             = Promise.promisify(require('figlet'));
 const morgan             = require('morgan');
 const express            = require("express");
 const db                 = require('../../models');//Bootstraps the entire database.
@@ -8,33 +11,50 @@ const serveStatic        = express.static;
 const PORT               = 8080;
 const middlwareError     = 'You made it to the no-no middleware. If you\'re confused, see:http://expressjs.com/en/guide/using-middleware.html';
 const IS_PRODUCTION      = false;
-db.sequelize
-  .sync()
-  .then(()=>{
+const introAscii         = '   POWr';
+const introAscii2        = 'Comments';
+const asciiFont          = 'Isometric2';
 
-    if (!IS_PRODUCTION) require('./bundler.js')(app); //Webpack
+figlet(introAscii,{font:asciiFont})
+.then(introMessage => {
+  console.log(introMessage.green);
+  return figlet(introAscii2,{font:asciiFont})
+})
+.then(introMessage => {
+  console.log(introMessage.green);
+})
+.then(nothing =>{
+  db.sequelize
+    .sync()
+    .then(()=>{
 
-    app.set('models',db.sequelize.models);
+      if (!IS_PRODUCTION) require('./bundler.js')(app); //Webpack
 
-    app.use(bodyParser.json({
-      extended:true //see:https://www.npmjs.com/package/body-parser
-    }));
+      app.set('models',db.sequelize.models);
 
-    app.get('/', function(req, res) {
-      res.sendFile(path.join(__dirname,"../../dist/index.html"));
-    });
+      app.use(bodyParser.json({
+        extended:true //see:https://www.npmjs.com/package/body-parser
+      }));
 
-    app.use(serveStatic(path.join(__dirname,"../../dist")));
+      app.get('/', function(req, res) {
+        res.sendFile(path.join(__dirname,"../../dist/index.html"));
+      });
 
-    app.use(function(req,res,next){// If you get here, then nothing was able to field the request.
-      res.send(JSON.stringify({
-        success:false,
-        data:middlwareError
-      }))
-    });
+      app.use(serveStatic(path.join(__dirname,"../../dist")));
 
-    app.listen(PORT, function () {
-      console.log('Server running on port ' + PORT);
-    });
-  }
-);
+      app.use(function(req,res,next){// If you get here, then nothing was able to field the request.
+        res.send(JSON.stringify({
+          success:false,
+          data:middlwareError
+        }))
+      });
+
+      app.listen(PORT, function () {
+        console.log('Server running on port ' + PORT);
+      });
+    }
+  );
+})
+
+
+
